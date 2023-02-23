360aa7910553cb7aba983241689c82fb165c3491
Anna Barton <anna.barton327@gmail.com>
2023-02-22 17:45:37 -0700

    lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
 
-function Keypad (){
+function Keypad() {
+
+    function handleEvent() {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleEvent} />
+        </div>
     )
 }
 
